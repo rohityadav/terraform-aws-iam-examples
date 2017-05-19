ee9e67ef3e01df2ed604e90a07b9d6b8ec5735cc
Rohit Yadav <rohit.yadav@manheim.com>
2017-05-19 00:04:31 -0400

    terraform example with AWS IAM servcies.

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "ec2-admin-role" {
+  name = "ec2-admin-role"
+
+  assume_role_policy="{\n    \"Version\": \"2012-10-17\",\n    \"Statement\": [\n      {\n        \"Action\": \"sts:AssumeRole\",\n        \"Principal\": {\n          \"Service\": \"ec2.amazonaws.com\"\n        },\n        \"Effect\": \"Allow\",\n        \"Sid\": \"\"\n      }\n    ]\n  }\n"
+}
+
+resource "aws_iam_policy" "admin-accss-policy" {
+  name = "admin-accss-policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": "*",
+          "Resource": "*"
+      }
+  ]
+}
+  EOF
+}
+
+resource "aws_iam_policy" "s3-access-policy" {
+  name = "s3-access-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": "s3:*",
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-s3-access-policy-attachment" {
+  policy_arn = "${aws_iam_policy.s3-access-policy.arn}"
+  role = "${aws_iam_role.ec2-admin-role.id}"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-admin-access-policy-attachment" {
+  policy_arn = "${aws_iam_policy.admin-accss-policy.arn}"
+  role = "${aws_iam_role.ec2-admin-role.id}"
+}
+
